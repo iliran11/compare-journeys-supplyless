f58455c52b397145e21dadde13fd939f59a3d31b
Liran Cohen <iliran11@gmail.com>
2026-08-18 10:34:40 +0300

    wip

--- app/config.js
+++ app/config.js
@@ -1,5 +1,7 @@
 export const BASE_URL = 'https://www.bookaway.com';
 
+export const DEPARTURE_TIME_WINDOW_PADDING_MINUTES = 5;
+
 export const PRESETS = [
   {
     name: 'Pinbus Colombia',
--- app/logic/buildFilterParams.js
+++ app/logic/buildFilterParams.js
@@ -1,8 +1,9 @@
 import padTime from './padTime';
+import { DEPARTURE_TIME_WINDOW_PADDING_MINUTES } from '../config';
 
 export default function buildFilterParams(groupRow) {
   const departureTime = groupRow.departure.slice(11);
-  const timeWindow = padTime(departureTime, -30) + '-' + padTime(departureTime, 30);
+  const timeWindow = padTime(departureTime, -DEPARTURE_TIME_WINDOW_PADDING_MINUTES) + '-' + padTime(departureTime, DEPARTURE_TIME_WINDOW_PADDING_MINUTES);
   const supplierParam = groupRow.supplierFilterId ? '&suppliers=' + encodeURIComponent(groupRow.supplierFilterId) : '';
   return { timeWindow, filterParams: supplierParam + '&departureTime=' + timeWindow };
 }
