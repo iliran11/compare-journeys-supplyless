c54b31cd8ffcdff32d936fc4a712ce03003cd961
Liran Cohen <iliran11@gmail.com>
2026-09-14 17:01:06 +0300

    fix: correct casing of 'departuredate' in URL construction

--- app/logic/buildBawResultsUrl.js
+++ app/logic/buildBawResultsUrl.js
@@ -1,5 +1,5 @@
 import { BASE_URL } from '../config';
 
 export default function buildBawResultsUrl(route, date, debugValue) {
-  return BASE_URL + '/s/' + route.countrySlug + '/' + route.fromSlug + '-to-' + route.toSlug + '?departureDate=' + date.trim() + '&debug=' + debugValue;
+  return BASE_URL + '/s/' + route.countrySlug + '/' + route.fromSlug + '-to-' + route.toSlug + '?departuredate=' + date.trim() + '&debug=' + debugValue;
 }
